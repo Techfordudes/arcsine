208107ae32b5f094a10b4f8937821a9e6809f465
Thidas Vinnath <thidasvinnath@gmail.com>
2024-10-05 01:44:39 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,105 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from "react";
+import * as math from "mathjs";// Import mathjs library for advanced calculations
+import Decimal from 'decimal.js';
 
-export default function Home() {
+function Home() {
+  const [arcsineValue, setArcsineValue] = useState("");
+  const [resultValue, setResultValue] = useState("");
+
+  const handleInputChange = (event: { target: { value: any; }; }) => {
+    const newValue = event.target.value;
+    setArcsineValue(newValue);
+  };
+
+  const calculateArcsine = () => {
+    // try {
+
+    const decimalValue = math.evaluate(arcsineValue);
+    const evaluatedValue = new Decimal(decimalValue);
+
+
+      if (evaluatedValue.toNumber() < -1 || evaluatedValue.toNumber() > 1) {
+        alert(
+          "Invalid input! Please enter a value between -1 and 1 for arcsine calculation."
+        );
+        return;
+      }
+
+      const resultDegrees = math.unit(math.asin(evaluatedValue), "rad").to("deg");
+      const roundedResult = new Decimal(resultDegrees.toString().replace(" deg", "")).toFixed(10);
+
+      const numberForMinutes = parseFloat(roundedResult);
+      const decimalPartForMinutes = numberForMinutes - Math.floor(numberForMinutes);
+      const degrees = parseFloat(roundedResult) - decimalPartForMinutes;
+      const minutesWithDecimals = decimalPartForMinutes * 60;
+
+      const numberForSeconds = minutesWithDecimals;
+      const decimalPartForSeconds = numberForSeconds - Math.floor(numberForSeconds);
+      const minutes = minutesWithDecimals - decimalPartForSeconds;
+      const secondsWithDecimals = decimalPartForSeconds * 60;
+
+      const roundedSeconds = Math.round(secondsWithDecimals);
+
+      setResultValue(`The Arcsine of ${arcsineValue} is approximately ${degrees} degrees and ${minutes} minutes and ${roundedSeconds} seconds`);
+
+    // } catch (error) {
+    //   console.error("Error evaluating expression:", error);
+    //   alert("Invalid expression! Please enter a valid mathematical expression.");
+    // }
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-full h-screen flex justify-center items-center p-10">
+      <div className=" flex justify-center items-center outline rounded-2xl p-10 gap-10 flex-col">
+        <label className="text-3xl">Arcsine Calculator</label>
+        <div className="flex lg:flex-row flex-col justify-between items-center gap-10">
+          <label htmlFor="">Arcsine Value:</label>
+          <input
+            type="text"
+            value={arcsineValue}
+            onChange={handleInputChange}
+            className="rounded-2xl outline-none text-black p-2"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <label htmlFor="">{resultValue}</label>
+        <button onClick={calculateArcsine} className="w-full text-lg border p-2 rounded-lg">
+          Calculate!
+        </button>
+
+      </div>
     </div>
   );
 }
+
+export default Home;
+
+
+// <div className="">
+// <h1>Arcsine Calculator</h1>
+// <table>
+//   <tbody>
+//     <tr>
+//       <td style={{ fontSize: "25px" }}>Arcsine Value:</td>
+//       <td>
+//         <input
+//           type="text"
+//           value={arcsineValue}
+//           onChange={handleInputChange}
+//           style={{ fontSize: "25px" }}
+//         />
+//       </td>
+//     </tr>
+//     <tr>
+//       <td>
+//         <button onClick={calculateArcsine} style={{ fontSize: "25px" }}>
+//           Calculate!
+//         </button>
+//       </td>
+//     </tr>
+//   </tbody>
+// </table>
+// <h1>
+//   {resultValue}
+// </h1>
+// </div>
